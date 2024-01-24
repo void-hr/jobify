d459bba8e9a42a4b6dc0df2b92a0e213934cffd7
void-hr <gsharmaji93@gmail.com>
2024-01-24 22:44:43 +0530

    added filter card component

---
+++ client/src/components/Filter/FilterCard.jsx
@@ -0,0 +1,32 @@
+import styles from './filtercard.module.css'
+const FilterCard = () => {
+    const skills = ['html', 'css', 'react', 'js']
+  return (
+    <div className={styles.card_container}>
+        <input type="text" />
+        <div className={styles.filter_keywords}>
+            <div className={styles.left_filter}>
+            <select defaultValue={"skills"}>
+                <option value="skills">Skills</option>
+                <option value="title">Title</option>
+
+            </select>
+            {skills.map((elem)=> (<Chip skill={elem}/>))}
+            </div>
+            <button className={styles.clear}>Clear</button>
+        </div>
+    </div>
+  )
+}
+
+export default FilterCard
+
+
+export const Chip = ({skill}) => {
+    return (
+        <div className={styles.chip}>
+            <p>{skill}</p>
+            <div className={styles.chip_x}>X</div>
+        </div>
+    )
+} 
--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -1,11 +1,13 @@
+import FilterCard from "../../components/Filter/FilterCard"
 import Navbar from "../../components/Navbar/Navbar"
 
 const Homepage = () => {
   return (
-    <div>
+    <div style={{display:"flex", flexDirection:'column', alignItems: 'center', gap:'4vh'}}>
         {/* list of component */}
         {/* 1. Navbar */}
      <Navbar />
+     <FilterCard /> 
         {/* 2. Search / filter Card */}
 
         {/* 3. Job Card */}
